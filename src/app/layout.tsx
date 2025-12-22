7fc7744a2db5cc52981189750a6757bec6bed9c0
Muhammad Irsyad Aulia <email-irsyadjokam313@gmail.com>
2025-12-22 09:35:47 +0700

    feat: Add Open Graph social media card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,40 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://elite-next-mu.vercel.app'),
   title: {
     template: "%s | EliteNext",
-    default: "EliteNext -  Dashboard & User Management",
+    default: "EliteNext -  The Ultimate User Dashboard",
   },
 
-  description: "A modern user management app built with Next.js, Tailwind, and Shadcn UI.",
-  keywords: ["Next.js", "React", "Dashboard", "User Management"],
-  authors: [{ name: "Muhammad_Irsyad_Aulia", url: "https://elite-next-mu.vercel.app" }],
+  description: "Modern user management platform with Next.js 15, Shadcn UI, and Tailwind CSS technologies.",
+  openGraph: {
+    title: {
+      template: "%s | EliteNext",
+      default: "EliteNext Dashboard",
+    },
+    description: "Modern user management platform with Next.js 15 technology.",
+    url: "/",
+    siteName: "EliteNext",
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1555066931-4365d14bab8c?q=80&w=1200&auto=format&fit=crop",
+        width: 1200,
+        height: 630,
+        alt: "EliteNext Dashboard Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  
+  twitter: {
+    card: "summary_large_image",
+    title: "EliteNext Dashboard",
+    description: "A modern user management platform made by Elite Developers.",
+    images: [
+      "https://images.unsplash.com/photo-1555066931-4365d14bab8c?q=80&w=1200&auto=format&fit=crop"],
+  },
 };
 
 export default function RootLayout({
