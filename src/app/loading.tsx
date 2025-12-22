0f8422ecf03498e3183f4037ad2959e1a67e712d
Muhammad Irsyad Aulia <email-irsyadjokam313@gmail.com>
2025-12-22 20:42:32 +0700

    feat: Add custom Favicon and Loading Skeleton UI

---
+++ src/app/loading.tsx
@@ -0,0 +1,27 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div className="p-10 max-w-5xl mx-auto space-y-4">
+      <div className="flex items-center space-x-4 mb-8">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {[...Array(4)].map((_, i) => (
+          <div key={i} className="flex flex-col space-y-3">
+            <Skeleton className="h-[125px] w-full rounded-xl" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
